a37a4cbd45bf1e266a423ee6098584d6cbb6857e
Akane Kurokawa <akanecco23@outlook.com>
2025-04-28 13:25:42 -0700

    Fix

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,9 +1,9 @@
 import fs from "node:fs";
 
-const template = fs.readFileSync("scripts/stats-template.md", "utf-8");
+let template = fs.readFileSync("scripts/stats-template.md", "utf-8");
 const stats = fs.readFileSync("stats", "utf-8");
 
-template.replace("{{stats}}", stats);
+template = template.replace("{{stats}}", stats);
 
 fs.writeFileSync("profile/README.md", template);
 
